60fe151d19cf9e9c1ac4b492a401f8a56381cc13
sam <shding@redhat.com>
2024-04-16 13:41:28 +0000

    update

--- test.sh
+++ test.sh
@@ -8,12 +8,7 @@ export REG="image-registry.openshift-image-registry.svc:5000"
 export PROJECT=$(oc project -q)
 export IMG="${REG}/${PROJECT}/hello"
 
-$ podman login --tls-verify=false --username ${USER} --password ${TKN} ${REG}
-Login Succeeded!
-$ podman build -t ${IMG} .
-(...)
-$ podman push --tls-verify=false ${IMG}
-(...)
-$ podman run --rm ${IMG}
-Hello from Kubedock!
-(...)
+ podman login --tls-verify=false --username ${USER} --password ${TKN} ${REG}
+ podman build -t ${IMG} .
+ podman push --tls-verify=false ${IMG}
+ podman run --rm ${IMG}
