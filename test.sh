40ad26ebc9a66e06ee68e6e2e89c8e9a2059affc
Sam Ding <samding@ca.ibm.com>
2026-05-28 11:31:54 -0400

    Simplify test.sh by removing Kubedock checks
    Removed conditional checks for KUBEDOCK_ENABLED and related logic.

--- test.sh
+++ test.sh
@@ -5,52 +5,7 @@ export KUBEDOCK_ENABLED=true
 echo "KUBEDOCK_ENABLED=$KUBEDOCK_ENABLED"
 
 # Run entrypoint script
-#/entry.sh
-if [[ "${KUBEDOCK_ENABLED:-false}" == "true" ]]; then
-    echo
-    echo "Kubedock is enabled (env variable KUBEDOCK_ENABLED is set to true)."
-
-    SECONDS=0
-    KUBEDOCK_TIMEOUT=${KUBEDOCK_TIMEOUT:-10}
-    until [ -f $KUBECONFIG ]; do
-        if (( SECONDS > KUBEDOCK_TIMEOUT )); then
-            break
-        fi
-        echo "Kubeconfig doesn't exist yet. Waiting..."
-        sleep 1
-    done
-
-    if [ -f $KUBECONFIG ]; then
-      echo "Kubeconfig found."
-
-      KUBEDOCK_PARAMS=${KUBEDOCK_PARAMS:-"--reverse-proxy --kubeconfig $KUBECONFIG"}
-
-      echo "Starting kubedock with params \"${KUBEDOCK_PARAMS}\"..."
-
-      kubedock server ${KUBEDOCK_PARAMS} > /tmp/kubedock.log 2>&1 &
-
-      echo "Done."
-
-      echo "Replacing podman with podman-wrapper.sh..."
-
-      ln -f -s /usr/bin/podman-wrapper.sh /home/tooling/.local/bin/podman
-
-      export TESTCONTAINERS_RYUK_DISABLED="true"
-      export TESTCONTAINERS_CHECKS_DISABLE="true"
-
-      echo "Done."
-      echo
-    else
-        echo "Could not find Kubeconfig at $KUBECONFIG"
-        echo "Giving up..."
-    fi
-else
-    echo
-    echo "Kubedock is disabled. It can be enabled with the env variable \"KUBEDOCK_ENABLED=true\""
-    echo "set in the workspace Devfile or in a Kubernetes ConfigMap in the developer namespace."
-    echo
-    ln -f -s /usr/bin/podman.orig /home/tooling/.local/bin/podman
-fi
+./entry.sh
 
 # Navigate to the project source directory
 cd "$PROJECT_SOURCE"
